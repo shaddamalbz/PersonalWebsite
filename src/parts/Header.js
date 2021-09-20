bb89bab83856ff7b770ca402bd11da005f1c6615
Shaddam Alghafiqih <shaddam.albz@gmail.com>
2021-09-20 22:28:29 +0700

    add menu for mobile screen

--- src/parts/Header.js
+++ src/parts/Header.js
@@ -15,31 +15,54 @@ export default function Header() {
   const { width } = useWindowDimension();
   const [isOpen, setIsOpen] = useState(false);
 
-  console.log(isOpen);
-
   if (width <= 1024)
     return (
-      <header className="w-full h-20 shadow-sm">
+      <header className="w-full h-20 shadow z-50">
         <div className="h-full sm:mx-6 md:mx-12 lg:mx-24 xl:mx-48 px-4 xl:px-0">
-          <nav className="h-full flex justify-between items-center text-lg">
-            <div className="flex items-center">
-              <p className="font-bold text-blue z-10">Shaddam</p>
-              <figure className="-ml-4 z-0">
-                <img src={BrandIconBg} alt="Blob" />
-              </figure>
+          <nav className="h-full">
+            <div className="h-full flex justify-between items-center text-lg">
+              <div className="flex items-center">
+                <p className="font-bold text-blue z-10">Shaddam</p>
+                <figure className="-ml-4 z-0">
+                  <img src={BrandIconBg} alt="Blob" />
+                </figure>
+              </div>
+              <span
+                className="text-blue text-2xl cursor-pointer"
+                onClick={() => setIsOpen(!isOpen)}
+              >
+                {!isOpen ? (
+                  <FontAwesomeIcon icon={faBars} />
+                ) : (
+                  <FontAwesomeIcon icon={faTimes} />
+                )}
+              </span>
             </div>
-            <span
-              className="text-blue text-2xl cursor-pointer"
-              onClick={() => setIsOpen(!isOpen)}
+            <ul
+              className={`${
+                !isOpen
+                  ? "hidden"
+                  : "bg-gray-50 flex flex-col items-center relative z-50 text-blue py-4 rounded-xl shadow-md mt-4"
+              }`}
             >
-              {!isOpen ? (
-                <FontAwesomeIcon icon={faBars} />
-              ) : (
-                <FontAwesomeIcon icon={faTimes} />
-              )}
-            </span>
+              <li className="py-2">Home</li>
+              <li className="py-2">About Me</li>
+              <li className="py-2">Portofolio</li>
+              <li className="py-2">Blog</li>
+              <li>
+                <Button type="button" isPrimary isRounded hasShadow>
+                  Contact Me
+                </Button>
+              </li>
+            </ul>
           </nav>
         </div>
+        {isOpen && (
+          <div
+            className="relative bg-black opacity-50 z-40"
+            style={{ minHeight: `calc(100vh - 5rem)` }}
+          ></div>
+        )}
       </header>
     );
 
